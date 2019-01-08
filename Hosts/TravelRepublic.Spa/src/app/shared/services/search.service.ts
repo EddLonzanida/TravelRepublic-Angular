3a5111081c3e2e37dc168cfcbdf82b03e9067be5
Edd Lonzanida <eddielonzanida@hotmail.com>
2019-01-08 14:33:15 +0800

    Updated serach service

--- Hosts/TravelRepublic.Spa/src/app/shared/services/search.service.ts
+++ Hosts/TravelRepublic.Spa/src/app/shared/services/search.service.ts
@@ -1,102 +1,94 @@
 import { Injectable, Inject } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams  } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Observable, of, throwError } from "rxjs";
 import { catchError, map, tap } from "rxjs/operators";
 
-import { MessageService } from "./message.service";
 import { SearchResponse } from "../responses/search-response";
 
 @Injectable(({ providedIn: "root" }) as any)
 export class SearchService {
-    private readonly baseUrl: string;
+  private readonly baseUrl: string;
 
-    constructor(private readonly httpClient: HttpClient, private messageService: MessageService, @Inject("BASE_URL") baseUrl: string) {
+  constructor(private readonly httpClient: HttpClient, @Inject("BASE_URL") baseUrl: string) {
 
-        this.baseUrl = baseUrl;
+    this.baseUrl = baseUrl;
 
-    }
+  }
 
-    getSuggestions(controller: string, query: string) {
+  getSuggestions(controller: string, query: string) {
 
-        const action = "suggestions";
-        const route = `${controller}/${action}`;
-        const param = {search: query};
+    const action = "suggestions";
+    const route = `${controller}/${action}`;
+    const param = { search: query };
 
-        return this.request<string[]>("getSuggestions", route, param);
-    }
+    return this.request<string[]>("getSuggestions", route, param);
+  }
 
-    search<TRequest, TResponse>(route: string, request: TRequest) {
+  search<TRequest, TResponse>(route: string, request: TRequest) {
 
-        const config = { params: request }
+    const config = { params: request }
 
-        return this.request<SearchResponse<TResponse>>("search", route, config);
+    return this.request<SearchResponse<TResponse>>("search", route, config);
 
-    }
+  }
 
-    request<TResponse>( operation: string, route: string, params?: any): Observable<TResponse> {
+  request<TResponse>(operation: string, route: string, params?: any): Observable<TResponse> {
 
-        const httpParams=this.toHttpParams(params);
-        const config = { params: httpParams }
-        const url = `${this.baseUrl}${route}`;
+    const httpParams = this.toHttpParams(params);
+    const config = { params: httpParams }
+    const url = `${this.baseUrl}${route}`;
 
-        return this.httpClient.get<TResponse>(url, config).pipe(
+    return this.httpClient.get<TResponse>(url, config).pipe(
 
-            // catchError(this.handleError<TResponse>(operation, {} as TResponse))
-            catchError(error => {
-                
-                this.log(`${operation} failed: ${error.message}`);
-                console.log("Handling error locally and rethrowing it...", error);
+      catchError(this.handleError<TResponse>(operation, {} as TResponse))
+      
+    );
+  }
 
-                //return Observable.throw(error);
-                return throwError(error);
 
-            })
-        );
-    }
+  private toHttpParams(obj: Object): HttpParams {
 
-    private toHttpParams(obj: Object): HttpParams {
-        
-        let params = new HttpParams();
+    let params = new HttpParams();
 
-        if (!obj) return params;
-        
-        for (const key in obj) {
+    if (!obj) return params;
 
-            if (obj.hasOwnProperty(key)) {
+    for (const key in obj) {
 
-                const val = obj[key];
+      if (obj.hasOwnProperty(key)) {
 
-                if (val !== null && val !== undefined) {
+        const val = obj[key];
 
-                    params = params.append(key, val.toString());
+        if (val !== null && val !== undefined) {
 
-                }
-            }
+          params = params.append(key, val.toString());
+
         }
-        return params;
+      }
     }
+    return params;
+  }
 
-    private handleError<T> (operation = "operation", result?: T) {
+  private log(message: string) {
 
-        return (error: any): Observable<T> => {
-       
-            // TODO: send the error to remote logging infrastructure
-            console.error(error); // log to console instead
+    console.log(message);
 
-            // TODO: better job of transforming error for user consumption
-            this.log(`${operation} failed: ${error.message}`);
+  }
 
-            // Let the app keep running by returning an empty result.
-            // return of(result as T);
-            return throwError(error);
 
-        };
-      }
+  private handleError<T>(operation = "operation", result?: T) {
 
-      private log(message: string) {
+    return (error: any): Observable<T> => {
 
-        console.log(message);
-       // this.messageService.add(`SearchService: ${message}`);
+      // TODO: send the error to remote logging infrastructure
+      // console.error(error); // log to console instead
 
-      }
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      // return of(result as T);
+      return throwError(error);
+
+    };
+  }
 }
